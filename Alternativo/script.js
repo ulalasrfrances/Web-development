e139ec975f58ed6cdd5788aa6f7e20d21f935e7f
Antony <adrojasa@uni.pe>
2021-03-15 11:38:18 -0500

    fsdfbsdfb

---
+++ Alternativo/script.js
@@ -0,0 +1,13 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+  
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
+
+/*document.querySelector('.container').addEventListener('click',()=>{document.querySelector('.navigation').classList})*/
