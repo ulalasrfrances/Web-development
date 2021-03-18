57f5eef4922d774ce7c4fd3a94a1762e1e70c147
Antony <adrojasa@uni.pe>
2021-03-18 12:27:33 -0500

    sip

--- Alternativo/script.js
+++ Alternativo/script.js
@@ -1,30 +1,31 @@
-const btnToggle = document.querySelector('.toggle-btn');
+$(document).ready(function() {
+
+  function toggleSidebar() {
+    $(".button").toggleClass("active");
+    $("main").toggleClass("move-to-left");
+    $(".sidebar-item").toggleClass("active");
+  }
+
+  $(".button").on("click tap", function() {
+    toggleSidebar();
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+      toggleSidebar();
+    }
+  });
+
+});
+
+/*const btnToggle = document.querySelector('.toggle-btn');
 
 btnToggle.addEventListener('click', function () {
   console.log('clik')
   document.getElementById('sidebar').classList.toggle('active');
   console.log(document.getElementById('sidebar'))
-});
-/*
-$(document).ready(function() {
+});*/
 
-    function toggleSidebar() {
-      $(".button").toggleClass("active");
-      $("main").toggleClass("move-to-left");
-      $(".sidebar-item").toggleClass("active");
-    }
-  
-    $(".button").on("click tap", function() {
-      toggleSidebar();
-    });
-  
-    $(document).keyup(function(e) {
-      if (e.keyCode === 27) {
-        toggleSidebar();
-      }
-    });
-  
-  });
-*/
-
-/*document.querySelector('.container').addEventListener('click',()=>{document.querySelector('.navigation').classList})*/
+ScrollReveal().reveal('.landscape');
+ScrollReveal().reveal('.navigation',{delay:500});
+ScrollReveal().reveal('#educacion',{delay:500});
